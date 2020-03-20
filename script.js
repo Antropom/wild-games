c3f5a6136eba34094432990f6e80c131a582dace
Louis Collet <colletlouis@gmail.com>
2020-03-20 13:52:40 +0100

    New darkmode button + littles fix

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-    }    
+        localStorage.setItem('theme', 'light');
+    }
 }
 
-const toggler = document.querySelector('#toggler');
+const toggler = document.querySelector('#checkbox');
 toggler.addEventListener('change', switchTheme, false);
