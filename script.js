26da51f317dff40ad30c6f32041073a8ffb395f6
PaulCrtr <paul.th.cartier@gmail.com>
2020-03-26 11:44:22 +0100

    recherche OK

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ function switchTheme(event) {
 
 const toggler = document.querySelector('#checkbox');
 toggler.addEventListener('change', switchTheme, false);
+
+
+/* Recherche filtrante */
+
+function runScript(e) {
+  const myIndex1 = ['resident', 'evil', 'resident evil 3 remake', 'resident evil remake', 'évil', 'résident']
+  const myIndex2 = ['cyberpunk', 'cyber punk', 'cyber', 'punk']
+  const myIndex3 = ['final fantasy 7 remake', 'final fantasy', 'final fantasy 7', 'final fantasy remake', 'final', 'fantasy']
+  const art1 = document.getElementById('search1')
+  const art2 = document.getElementById('search2')
+  const art3 = document.getElementById('search3')
+    if (e.keyCode == 13) {
+        var tb = document.getElementById("recherche");
+        let mySearch = tb.value.toLowerCase()
+        if (myIndex1.includes(mySearch)){
+            art2.style.display = 'none'
+            art3.style.display = 'none'
+            art1.style.display = 'flex'
+        } else if (myIndex2.includes(mySearch)){
+            art1.style.display = 'none'
+            art3.style.display = 'none'
+            art2.style.display = 'flex'
+        } else if (myIndex3.includes(mySearch)){
+            art1.style.display = 'none'
+            art2.style.display = 'none'
+            art3.style.display = 'flex'
+        } else {
+            art1.style.display = 'flex'
+            art2.style.display = 'flex'
+            art3.style.display = 'flex'
+        }
+        return false;
+    }
+}
